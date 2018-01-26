d597d0a01a5ff6db372961ec3f23df2646e9a584
ecjon <340651973@qq.com>
2018-01-26 09:56:14 +0800

    singal gpu control

---
+++ gpuFansDaemon.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+DISPLAY=:0 nvidia-settings -a "[gpu:0]/GPUFanControlState=1"
+
+temp=`nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader`
+
+control_unit=`echo "scale=3;100 / 70"|bc`
+
+while [ 1 ];do
+	sleep 1s
+	tmpf=`echo "$temp * $control_unit"|bc`
+	tmp=`printf "%1.0f" $tmpf`
+	echo $tmp
+	DISPLAY=:0 nvidia-settings -a [fan:0]/GPUTargetFanSpeed=$tmp
+done
